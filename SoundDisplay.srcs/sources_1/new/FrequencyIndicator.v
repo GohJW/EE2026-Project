6935ed30e55917400abede105989a0a10e3bd2bc
GohJW <aarongjw00@gmail.com>
2022-10-20 18:32:39 +0800

    quite accurate now but slow

--- SoundDisplay.srcs/sources_1/new/FrequencyIndicator.v
+++ SoundDisplay.srcs/sources_1/new/FrequencyIndicator.v
@@ -28,7 +28,27 @@ module FrequencyIndicator(
     );
     reg[31:0] timer = 0;
     reg[31:0] count = 0;
-    reg [31:0] multiply = 0;
+    reg[31:0] multiply = 0;
+    reg[3:0] sample_state = 0;
+    reg[31:0] sample1 = 0;
+    reg[31:0] sample2 = 0;
+    reg[31:0] sample3 = 0;
+    reg[31:0] sample4 = 0;
+    reg[31:0] sample5 = 0;
+    reg[31:0] sample6 = 0;
+    reg[31:0] sample7 = 0;
+    reg[31:0] sample8 = 0;
+    reg[31:0] sample9 = 0;
+    reg[31:0] sample10 = 0;
+    
+    reg[31:0] average1 = 0;
+    reg[31:0] average2 = 0;
+    reg[31:0] average3 = 0;
+    reg[31:0] average4 = 0;
+    reg[31:0] average5 = 0;
+    reg[31:0] average6 = 0;
+    reg[31:0] average7 = 0;
+    
     always@ (posedge clk_20k) begin
     multiply <= MIC_in * 10;
     timer <= (timer == 2000) ? 0:timer+1;
@@ -40,8 +60,50 @@ module FrequencyIndicator(
             cross <= 0;
         end
         if(timer == 2000) begin
-            frequency <= count*10;
+            case(sample_state)
+            4'b0000: begin
+                sample1 <= count*10;
+            end
+            4'b0001: begin
+                sample2 <= count*10;
+            end
+            4'b0010: begin
+                sample3 <= count*10;
+            end
+            4'b0011: begin
+                sample4 <= count*10;
+            end
+            4'b0100: begin
+                sample5 <= count*10;
+            end
+            4'b0101: begin
+                sample6 <= count*10;
+            end
+            4'b0110: begin
+                sample7 <= count*10;
+            end
+            4'b0111: begin
+                sample8 <= count*10;
+            end
+            4'b1000: begin
+                sample9 <= count*10;
+            end
+            4'b1001: begin
+                sample10 = count*10;
+                average1 = (sample1 + sample2)/2;
+                average2 = (sample3 + sample4)/2;
+                average3 = (sample5 + sample6)/2;
+                average4 = (sample7 + sample8)/2;
+                average5 = (sample9 + sample10)/2;
+                average6 = (average1 + average2)/2;
+                average7 = (average3 + average4 + average5)/3;
+                frequency = (average6 + average7)/2;
+//                average8 <= (average5 + average6)/2;
+//                average9 <= (average7 + average8)/2;
+            end
+            endcase
             count <= 0;
+            sample_state <= (sample_state == 4'b1001) ? 0:sample_state + 1;
         end
     end    
 endmodule
