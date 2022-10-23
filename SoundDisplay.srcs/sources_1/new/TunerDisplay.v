bf4b9f743790312dc102fcda83bb3dbd8dfa249a
Ju-Can <ju.can.0860@gmail.com>
2022-10-24 05:47:36 +0800

    C4 tuner screen cut by x (Failed)

--- SoundDisplay.srcs/sources_1/new/TunerDisplay.v
+++ SoundDisplay.srcs/sources_1/new/TunerDisplay.v
@@ -29,24 +29,110 @@ module TunerDisplay(
     );
     
     always@(posedge clock_6p25m) begin
-        if(frequency >= 254 && frequency < 269) begin
-            if(((y > 23 && y < 25)||(y>49 && y<51)) && (x > 37 && x < 57)) // start of C
-                oled_data <= 16'b1111111111111111;
-            else if(((y == 25)||(y == 49)) && (x > 36 && x < 58))
-                oled_data <= 16'b1111111111111111;
-            else if(((y == 26) || (y == 48)) && (x > 36 && x < 59))
-                oled_data <= 16'b1111111111111111;
-            else if(((y > 26 && y < 29)||(y >45 && y<48)) && ((x >35 && x < 40) || (x>55 && x<59)))
-                oled_data<= 16'b1111111111111111;
-            else if((y > 28 && y < 46) && (x>35 && x<40))
-                oled_data <=16'b1111111111111111; // end of C
-            else
-                oled_data<=16'b0000000000000000;
+        if(frequency >= 259 && frequency <= 263) begin // C4 in tune
+            // pointer
+            if ( x == 47 && x == 48 && y >= 9 && y <= 24 ) begin
+                oled_data <= 16'b0000011111100000; end
+            // letter
+            else if( x == 43 && y >= 39 && y <= 50 ) begin // start of C
+                oled_data <= 16'b1111111111111111; end
+            else if( x == 44 && y >= 37 && y <= 52) begin
+                oled_data <= 16'b1111111111111111; end
+            else if( x == 45 && y >= 35 && y <= 54) begin
+                oled_data <= 16'b1111111111111111; end
+            else if ( x >= 46 && x <= 52 && y == 36 && y == 37 && y == 52 && y == 53 ) begin
+                oled_data <= 16'b1111111111111111; end
+            else if ( x == 53 && ((y >= 36 && y <= 39) || ( y >= 50 && y <= 53 ))) begin
+                oled_data <= 16'b1111111111111111; end
+            else if ( x == 54 && ((y >= 37 && y <= 39) || ( y >= 50 && y <= 52))) begin
+                oled_data <= 16'b1111111111111111; end
+            // number 4
+            else if( x == 34 && x == 35 && y >= 47 && y <= 54 ) begin
+                oled_data<= 16'b1111111111111111; end
+            else if( x == 36 && x == 37 && y == 53  && y == 54 ) begin
+                oled_data <=16'b1111111111111111; end
+            else if ( x == 38 && x == 39 && y >= 47 && y <= 57 ) begin
+                oled_data <=16'b1111111111111111; end
+            else if ( x == 40 && y == 53 && y == 54) begin
+                oled_data <=16'b1111111111111111; end   
+            else begin
+                oled_data<=16'b0000000000000000; end
         end
-        else if(frequency >= 300 && frequency < 400) begin
-            oled_data <= 16'b0000011111100000;
+        else if(frequency >= 253 && frequency <= 259) begin // C4 flat
+            // pointer
+            if ( x == 21 && x == 22 && y >= 9 && y <= 11 ) begin
+                oled_data <=16'b1111100000000000; end 
+            else if ( x == 23 && x == 24 && y >= 12 && y <= 14 )begin
+                oled_data <=16'b1111100000000000; end
+            else if ( x == 25 && x == 26 && y >= 15 && y <= 17 ) begin
+                oled_data <=16'b1111100000000000; end 
+            else if ( x == 27 && x == 28 && y >= 18 && y <= 20 ) begin
+                oled_data <=16'b1111100000000000; end 
+            else if ( x == 29 && x == 30 && y >= 21 && y <= 23) begin
+                oled_data <=16'b1111100000000000; end
+            // letter
+            else if( x == 43 && y >= 39 && y <= 50 ) begin // start of C
+                oled_data <= 16'b1111111111111111; end
+            else if( x == 44 && y >= 37 && y <= 52) begin
+                oled_data <= 16'b1111111111111111; end
+            else if( x == 45 && y >= 35 && y <= 54) begin
+                oled_data <= 16'b1111111111111111; end
+            else if ( x >= 46 && x <= 52 && y == 36 && y == 37 && y == 52 && y == 53 ) begin
+                oled_data <= 16'b1111111111111111; end
+            else if ( x == 53 && ((y >= 36 && y <= 39) || ( y >= 50 && y <= 53 ))) begin
+                oled_data <= 16'b1111111111111111; end
+            else if ( x == 54 && ((y >= 37 && y <= 39) || ( y >= 50 && y <= 52))) begin
+                oled_data <= 16'b1111111111111111; end
+            // number 4
+            else if( x == 34 && x == 35 && y >= 47 && y <= 54 ) begin
+                oled_data<= 16'b1111111111111111; end
+            else if( x == 36 && x == 37 && y == 53  && y == 54 ) begin
+                oled_data <=16'b1111111111111111; end
+            else if ( x == 38 && x == 39 && y >= 47 && y <= 57 ) begin
+                oled_data <=16'b1111111111111111; end
+            else if ( x == 40 && y == 53 && y == 54) begin
+                oled_data <=16'b1111111111111111; end   
+            else begin
+                oled_data<=16'b0000000000000000; end
+        end
+        else if ( frequency >= 264 && frequency <= 269 ) begin
+            // pointer
+            if ( x == 74 && x == 75 && y >= 9 && y <= 11 ) begin
+                oled_data <=16'b1111100000000000; end 
+            else if ( x == 72 && x == 73 && y >= 12 && y <= 14 )begin
+                oled_data <=16'b1111100000000000; end
+            else if ( x == 70 && x == 71 && y >= 15 && y <= 17 ) begin
+                oled_data <=16'b1111100000000000; end 
+            else if ( x == 68 && x == 69 && y >= 18 && y <= 20 ) begin
+                oled_data <=16'b1111100000000000; end 
+            else if ( x == 66 && x == 67 && y >= 21 && y <= 23) begin
+                oled_data <=16'b1111100000000000; end
+            // letter
+            else if( x == 43 && y >= 39 && y <= 50 ) begin // start of C
+                oled_data <= 16'b1111111111111111; end
+            else if( x == 44 && y >= 37 && y <= 52) begin
+                oled_data <= 16'b1111111111111111; end
+            else if( x == 45 && y >= 35 && y <= 54) begin
+                oled_data <= 16'b1111111111111111; end
+            else if ( x >= 46 && x <= 52 && y == 36 && y == 37 && y == 52 && y == 53 ) begin
+                oled_data <= 16'b1111111111111111; end
+            else if ( x == 53 && ((y >= 36 && y <= 39) || ( y >= 50 && y <= 53 ))) begin
+                oled_data <= 16'b1111111111111111; end
+            else if ( x == 54 && ((y >= 37 && y <= 39) || ( y >= 50 && y <= 52))) begin
+                oled_data <= 16'b1111111111111111; end
+            // number 4
+            else if( x == 34 && x == 35 && y >= 47 && y <= 54 ) begin
+                oled_data<= 16'b1111111111111111; end
+            else if( x == 36 && x == 37 && y == 53  && y == 54 ) begin
+                oled_data <=16'b1111111111111111; end
+            else if ( x == 38 && x == 39 && y >= 47 && y <= 57 ) begin
+                oled_data <=16'b1111111111111111; end
+            else if ( x == 40 && y == 53 && y == 54) begin
+                oled_data <=16'b1111111111111111; end   
+            else begin
+                oled_data<=16'b0000000000000000; end
         end
         else
-            oled_data <= 16'b0000000000000000;
+            oled_data <= 16'b0000000000011111;
     end
 endmodule
